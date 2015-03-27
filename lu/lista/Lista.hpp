749c0487ecaf9e6e7bc8b731d70aa6745f57e28f
luciano <marcelinolucianom@gmail.com>
2015-03-27 20:19:49 -0300

    agora sim

--- lu/lista/Lista.hpp
+++ lu/lista/Lista.hpp
@@ -4,128 +4,131 @@
 
 template <typename T>
 
- class Lista {
- private:
-  int fimDaLista;
-  int maximoDeElementos;
-  T* dados = new T[maximoDeElementos];
+  class Lista {
+   private:
+    int fimDaLista;
+    int maximoDeElementos;
+    T* dados = new T[maximoDeElementos];
 
-  public:
- 	Lista() {
+   public:
+    Lista() {
      maximoDeElementos = 10;
      inicializaLista();
-  }
-
-  explicit Lista(int tam) {
-    maximoDeElementos = tam;
-    inicializaLista();
-  }
-
-  void adiciona(T dado) {
-    if (listaCheia()) {
-      throw "Lista Cheia"
-    }
-    adicionaNaPosicao(dado, fimDaLista); //SE A LISTA ESTIVER VAZIA, VAI
-    //COLOCAR NA POSICAO[-1]
-  }
-
-  void adicionaNoInicio(T dado) {
-    adicionaNaPosicao(dado, 0);
-  }
-
-  void adicionaNaPosicao(T dado, int posicao) {
-    if (listaCheia()) {
-      throw "Lista Cheia";
-    }
-    if (posicao > fimDaLista || posicao < 0) {
-      throw "Erro de Posicao"
     }
 
-    fimDaLista++;
-    int ultimoDaLista = fimDaLista;
-
-    for (ultimoDaLista; ultimoDaLista > posicao; ultimoDaLista--) {
-      dados[ultimoDaLista] = dados[ultimoDaLista - 1];
+    explicit Lista(int tam) {
+      maximoDeElementos = tam;
+      inicializaLista();
     }
-    dados[posicao] = dado;
-  }
 
-  void adicionaEmOrdem(T dado) {}
-
-  T retira() {
-    return retiraDaPosicao(fimDaLista);
-  }
-
-  T retiraDoInicio() {
-    return retiraDaPosicao(posicao(0));
-  }
-
-  T retiraDaPosicao(int posicao) {
-    if (listaVazia()) {
-      throw "Lista Vazia";
+    T::operator<(T dado1, T dado2) {
+      return static_cast<int>(dado1) > static_cast<int>(dado2);
     }
-    T retirado = dados[posicao];
-    int contador = posicao;
-    for (contador; contador < fimDaLista; contador++) {
-      dados[contador] = dados[contador + 1];
+
+    void adiciona(T dado) {
+      if (listaCheia()) {
+        throw "Lista Cheia";
+      }
+      adicionaNaPosicao(dado, fimDaLista);
     }
-    fimDaLista--;
-    return retirado;
-  }
 
-  T retiraEspecifico(T dado) {
-    return retiraDaPosicao(posicao(dado));
-  }
+    void adicionaNoInicio(T dado) {
+      adicionaNaPosicao(dado, 0);
+    }
 
-  int posicao(T dado) {
-   int posicao = 0;
-   while (posicao <= fimDaLista && (dado != dados[posicao])) {
-     posicao += 1;
-   }
-   if(posicao > fimDaLista) {
-     throw "Item não encontrado";
-   } else {
-     return posicao;
-   }
-  }
+    void adicionaNaPosicao(T dado, int posicao) {
+      if (listaCheia()) {
+        throw "Lista Cheia";
+      }
+      if (posicao > fimDaLista || posicao < 0) {
+        throw "Erro de Posicao"
+      }
 
-  bool contem(T dado) {
-   if (listaVazia()) {
-     throw "Lista Vazia";
-   }
-   for (int contador = 0; contador < fimDaLista; contador++) {
-     if (dados[contador] == dado) {
-       return true;
-     }
-   }
-   return false;
-  }
+      fimDaLista++;
+      int ultimoDaLista = fimDaLista;
 
-  bool igual(T dado1, T dado2) {
-   return if (dado1 == dado2);
-  }
+      for (ultimoDaLista; ultimoDaLista > posicao; ultimoDaLista--) {
+        dados[ultimoDaLista] = dados[ultimoDaLista - 1];
+      }
+      dados[posicao] = dado;
+    }
 
-  bool maior(T dado1, T dado2) {
-   return if (dado1 > dado2);
-  }
+    void adicionaEmOrdem(T dado) {}
 
-  bool menor(T dado1, T dado2) {
-   return if (dado1 < dado2);
-  }
+    T retira() {
+      return retiraDaPosicao(fimDaLista);
+    }
 
-  bool listaCheia() {
-   return if (fimDaLista == maximoDeElementos);
-  }
+    T retiraDoInicio() {
+      return retiraDaPosicao(posicao(0));
+    }
 
-  bool listaVazia() {
-   return if (fimDaLista == -1);
-  }
+    T retiraDaPosicao(int posicao) {
+      if (listaVazia()) {
+        throw "Lista Vazia";
+      }
+      T retirado = dados[posicao];
+      int contador = posicao;
+      for (contador; contador < fimDaLista; contador++) {
+        dados[contador] = dados[contador + 1];
+      }
+      fimDaLista--;
+      return retirado;
+    }
 
-  void inicializaLista() {
-   fimDaLista = -1;
-  }
+    T retiraEspecifico(T dado) {
+      return retiraDaPosicao(posicao(dado));
+    }
 
-  void destroiLista() {
-   fimDaLista = -1;
-  }
- };
+    int posicao(T dado) {
+      int posicao = 0;
+      while (posicao <= fimDaLista && (dado != dados[posicao])) {
+        posicao += 1;
+      }
+      if (posicao > fimDaLista) {
+        throw "Item não encontrado";
+      } else {
+        return posicao;
+      }
+    }
+
+    bool contem(T dado) {
+      if (listaVazia()) {
+        throw "Lista Vazia";
+      }
+      for (int contador = 0; contador < fimDaLista; contador++) {
+        if (dados[contador] == dado) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    bool igual(T dado1, T dado2) {
+      return dado1 == dado2;
+    }
+
+    bool maior(T dado1, T dado2) {
+      return dado1 > dado2;
+    }
+
+    bool menor(T dado1, T dado2) {
+      return dado1 < dado2;
+    }
+
+    bool listaCheia() {
+      return fimDaLista == maximoDeElementos;
+    }
+
+    bool listaVazia() {
+      return fimDaLista == -1;
+    }
+
+    void inicializaLista() {
+      fimDaLista = -1;
+    }
+
+    void destroiLista() {
+      fimDaLista = -1;
+    }
+  };
