f216244565cac4cce49f12300b33b84a61094618
luciano <marcelinolucianom@gmail.com>
2015-03-27 18:56:32 -0300

    initialize lista.hpp

---
+++ lu/lista/Lista.hpp
@@ -0,0 +1,21 @@
+ class Lista {
+  public:
+ 	Lista()
+ 	explicit Lista(int tam)
+ 	void adiciona(T dado)
+ 	void adicionaNoInicio(T dado)
+ 	void adicionaNaPosicao(T dado, int posicao)
+ 	void adicionaEmOrdem(T dado)
+ 	T retira()
+ 	T retiraDoInicio()
+ 	T retiraDaPosicao(int posicao)
+ 	T retiraEspecifico(T dado)
+ 	int posicao(T dado)
+ 	bool contem(T dado)
+ 	bool igual(T dado1, T dado2)
+ 	bool maior(T dado1, T dado2)
+ 	bool menor(T dado1, T dado2)
+ 	bool listaCheia()
+ 	bool listaVazia()
+ 	void destroiLista()
+ };
