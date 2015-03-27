b18d0c1515578a812b50aadd06d3b7ebc53f490d
luciano <marcelinolucianom@gmail.com>
2015-03-27 19:18:16 -0300

    boolean functions

--- lu/lista/Lista.hpp
+++ lu/lista/Lista.hpp
@@ -6,12 +6,16 @@ template <typename T>
 
  class Lista {
  private:
-   int fimDaLista;
-   int maximoDeElementos;
-   T* dados = new T[maximoDeElementos];
+  int fimDaLista;
+  int maximoDeElementos;
+  T* dados = new T[maximoDeElementos];
 
   public:
- 	Lista()
+ 	Lista() {
+     maximoDeElementos = 10;
+     inicializaLista();
+  }
+
  	explicit Lista(int tam)
  	void adiciona(T dado)
  	void adicionaNoInicio(T dado)
@@ -22,11 +26,44 @@ template <typename T>
  	T retiraDaPosicao(int posicao)
  	T retiraEspecifico(T dado)
  	int posicao(T dado)
- 	bool contem(T dado)
- 	bool igual(T dado1, T dado2)
- 	bool maior(T dado1, T dado2)
- 	bool menor(T dado1, T dado2)
- 	bool listaCheia()
- 	bool listaVazia()
- 	void destroiLista()
+
+  bool contem(T dado) {
+   if (listaVazia()) {
+     throw "Lista Vazia";
+   }
+   for (int contador = 0; contador < fimDaLista; contador++) {
+     if (dados[contador] == dado) {
+       return true;
+     }
+   }
+   return false;
+  }
+
+  bool igual(T dado1, T dado2) {
+   return if (dado1 == dado2);
+  }
+
+  bool maior(T dado1, T dado2) {
+   return if (dado1 > dado2);
+  }
+
+  bool menor(T dado1, T dado2) {
+   return if (dado1 < dado2);
+  }
+
+  bool listaCheia() {
+   return if (fimDaLista == maximoDeElementos);
+  }
+
+  bool listaVazia() {
+   return if (fimDaLista == -1);
+  }
+
+  void inicializaLista() {
+   fimDaLista = -1;
+  }
+
+  void destroiLista() {
+   fimDaLista = -1;
+  }
  };
