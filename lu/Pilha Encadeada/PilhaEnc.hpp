090b15296aff001e618014145241b7dc51ee50d4
luciano <marcelinolucianom@gmail.com>
2015-04-08 20:22:57 -0300

    feito em aula

--- lu/Pilha Encadeada/PilhaEnc.hpp
+++ lu/Pilha Encadeada/PilhaEnc.hpp
@@ -1,3 +1,19 @@
 pesquisar no google:
 how to remap a method in c++ with private inheritance
 calling inherited constructor
+
+template<typename T>
+class PilhaEnc: private ListaEnc<T>{
+ public:
+    PilhaEnc() : LiscaEnc() {}
+    ~PilhaEnc() {
+      limparPilha();
+    }
+    void empilha(const T& dado): adicionaNoInicio(const T& dado) {}
+    T desempilha(): retiraDoInicio() {}
+	T topo() {
+    // implementar
+  }
+	void limparPilha(): ~ListaEnc() {}
+	bool PilhaVazia(): listaVazia() {}
+};
