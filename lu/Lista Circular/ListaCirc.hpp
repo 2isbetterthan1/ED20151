03b5b588f2e73912886192cd0b3b12b3e9cd8154
luciano <marcelinolucianom@gmail.com>
2015-04-17 20:08:39 -0300

    experimento

--- lu/Lista Circular/ListaCirc.hpp
+++ lu/Lista Circular/ListaCirc.hpp
@@ -2,27 +2,94 @@
  * Caboclo.hpp
  */
 
-class ListaCirc {
+
+#include "Elemento.hpp"
+#include "ListaEnc.hpp"
+
+template<typename T>
+
+class ListaCirc : public ListaEnc<T> {
+ private:
+	ListaEnc<T>* lista;
+	Elemento<T>* sentinela;
+
  public:
-	ListaCirc()
-	~ListaCirc()
-	void adicionaNoInicio(const T& dado)
-	T retiraDoInicio()
-	void eliminaDoInicio()
-	void adicionaNaPosicao(const T& dado, int pos)
-	int posicao(const T& dado) const
-	T* posicaoMem(const T& dado) const
-	bool contem(const T& dado)
-	T retiraDaPosicao(int pos)
-	void adiciona(const T& dado)
-	T retira()
-	T retiraEspecifico(const T& dado)
-	T mostra(int pos)
-	void adicionaEmOrdem(const T& data)
-	int verUltimo()
-	bool listaVazia() const
-	bool igual(T dado1, T dado2)
-	bool maior(T dado1, T dado2)
-	bool menor(T dado1, T dado2)
-	void destroiLista()
+	ListaCirc(): ListaEnc<T>::ListaEnc() {
+		lista = new ListaEnc<T>();
+	}
+	~ListaCirc() {
+		ListaEnc<T>::~ListaEnc();
+	}
+	void adicionaNoInicio(const T& dado);
+	T retiraDoInicio() {
+			//  return ListaEnc<T>::retiraDoInicio();
+		return retiraDoInicio();
+	}
+	void eliminaDoInicio() {  //  REESCREVER
+		eliminaDoInicio();
+	}
+
+	void adicionaNaPosicao(const T& dado, int pos) {
+		ListaEnc<T>::adicionaNaPosicao(dado, pos);
+	}
+
+	int posicao(const T& dado) const {
+		return ListaEnc<T>::posicao(dado);
+	}
+
+	T* posicaoMem(const T& dado) const {
+		return ListaEnc<T>::posicaoMem(dado);
+	}
+
+	bool contem(const T& dado) {
+		return ListaEnc<T>::contem(dado);
+	}
+
+	T retiraDaPosicao(int pos) {
+		return ListaEnc<T>::retiraDaPosicao(pos);
+	}
+
+	void adiciona(const T& dado) {  //  REESCREVER
+		ListaEnc<T>::adiciona(dado);
+	}
+
+	T retira() {  //  REESCREVER
+		return ListaEnc<T>::retira();
+	}
+
+	T retiraEspecifico(const T& dado) {
+		return ListaEnc<T>::retiraEspecifico(dado);
+	}
+
+	T mostra(int pos) {  //  ESCREVER
+		return;
+	}
+
+	void adicionaEmOrdem(const T& dado) {
+		ListaEnc<T>::adicionaEmOrdem(dado);
+	}
+
+	int verUltimo() {
+		return 0;
+	}
+
+	bool listaVazia() const {
+		return ListaEnc<T>::listaVazia();
+	}
+
+	bool igual(T dado1, T dado2) {
+		return ListaEnc<T>::igual(dado1, dado2);
+	}
+
+	bool maior(T dado1, T dado2) {
+		return ListaEnc<T>::maior(dado1, dado2);
+	}
+
+	bool menor(T dado1, T dado2) {
+		return ListaEnc<T>::menor(dado1, dado2);
+	}
+
+	void destroiLista() {
+		ListaEnc<T>::destroiLista();
+	}
 };
