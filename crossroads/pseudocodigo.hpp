4dd4b97cf5af800c6cf825b799737c630202b033
luciano <marcelinolucianom@gmail.com>
2015-04-22 21:48:30 -0300

    tacale pau

--- crossroads/pseudocodigo.hpp
+++ crossroads/pseudocodigo.hpp
@@ -1,19 +1,117 @@
-Controlador de Eventos {
+#define TEMPOSEMAFORO 15
+
+controladorDeEventos() {
+
+private:
+  int tempoTotal;
+  int tempoSemaforo = TEMPOSEMAFORO;
+
+public:
+  controladorDeEventos(tempoTotal) {
+    this->tempoTotal = tempoTotal;
+  }
+
+
+  CriaLinhaDoTempo() {
+    ListaEnc linhaDoTempo = new ListaEnc<Evento>();
+  }
 
   InicializaPista() {
-    Pista p1 = new Pista(...);
+    Fila p1 = new Fila<Carro>(...);
     p1.criaPilhaAleatoria(8, 1, 1, p2, p3, p4);
     (p2,p3,p4...)
   }
 
   GeraEventosIniciais() {
-    
+    geraSemaforos();
+    geraCarros();
+  }
+
+  geraSemaforos() {
+    int tempoCorrente = 0;
+    while(tempoCorrente < tempoTotal) {
+      GeraSemaforo semaforo = new GeraSemaforo(tempoCorrente);
+      linhaDoTempo->adicionaEmOrdem(semaforo);
+      tempoCorrente += tempoSemaforo;
+    }
+  }
+
+  geraCarros() {
+    // int ordemCronologica = 0;
+    // while(ordemCronologica < 15) {
+    //   GeraCarro carro1 = new geraCarro(p1, ordemCronologica);
+    //   linhaDoTempo->adicionaEmOrdem(carro1);
+    //   ordemCronologica += 5;
+    // }
+
+    GeraCarro carro1 = new geraCarro(p1, 0);
+    linhaDoTempo->adicionaEmOrdem(carro1)
+
+    GeraCarro carro2 = new geraCarro(p2, 0);
+    linhaDoTempo->adicionaEmOrdem(carro2);
+    (p3, p4, ...)
+  }
+
+
+
+
+}
+
+abstract Evento() {
+  Evento() { }
+}
+
+GeraCarro() : Evento() {
+
+private:
+  int tempo;
+  Pista pista;
+public:
+  GeraCarro(pista, tempo) {
+    this->tempo = tempo;
+    this->pista = pista;
+  }
+
+  insereCarro() {
+    Carro carro = new Carro();
+    pista->adiciona(carro);
+  }
+
+  geraProximoCarro() {
+    int proximoTempo = tempo + pista->getFrequenciaAddCarro();
+
+    if(proximoTempo < tempoTotal) { //  VERIFICAR SE TEMOS ACESSO AO TEMPO TOTAL.
+      GeraCarro novoCarro = new GeraCarro(pista, proximoTempo);
+    }
+  }
+
+}
+
+GeraSemaforo(int tempo) : Evento() {
+private:
+  int tempo;
+
+public:
+  GeraSemaforo(tempo) {
+    this->tempo = tempo;
   }
 
 
 }
 
-Pista {
+ChegaSemaforo() : Evento() {
+
+}
+
+Cruza() : Evento() {
+
+}
+
+
+
+Pista() {
+
+  int frequenciaEntradaDeCarros;
 
   criaPilhaAleatoriaX(prob1, prob2, prob3, primeiraOpcao, segundaOpcao, terceiraOpcao) {
     Pilha aleatorio = new Pilha;
@@ -30,3 +128,5 @@ Pista {
   }
 
 }
+
+Carro() {}
