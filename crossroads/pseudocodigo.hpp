6b96ef34f047534d4b5a98dbfae69d7e4020ca1a
luciano <marcelinolucianom@gmail.com>
2015-04-22 21:11:57 -0300

    init pseudocodigo

---
+++ crossroads/pseudocodigo.hpp
@@ -0,0 +1,32 @@
+Controlador de Eventos {
+
+  InicializaPista() {
+    Pista p1 = new Pista(...);
+    p1.criaPilhaAleatoria(8, 1, 1, p2, p3, p4);
+    (p2,p3,p4...)
+  }
+
+  GeraEventosIniciais() {
+    
+  }
+
+
+}
+
+Pista {
+
+  criaPilhaAleatoriaX(prob1, prob2, prob3, primeiraOpcao, segundaOpcao, terceiraOpcao) {
+    Pilha aleatorio = new Pilha;
+    for(prob1) {
+      aleatorio.empilha(primeiraOpcao);
+    }
+    for(prob2) {
+      aleatorio.empilha(segundaOpcao);
+    }
+    for(prob3) {
+      aleatorio.empilha(terceiraOpcao);
+    }
+    return aleatorio
+  }
+
+}
