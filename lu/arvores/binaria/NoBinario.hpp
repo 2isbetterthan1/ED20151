d2c47dc0c69eb25d20aa983281d1487f343079cb
luciano <marcelinolucianom@gmail.com>
2015-07-05 11:37:26 -0300

    tava errada porra

--- lu/arvores/binaria/NoBinario.hpp
+++ lu/arvores/binaria/NoBinario.hpp
@@ -1,7 +1,6 @@
 /* Copyright [2015] <marcelinol>
  * NoBinario.hpp
  */
-
 #ifndef NO_BINARIO_HPP
 #define NO_BINARIO_HPP
 
@@ -11,144 +10,166 @@
 template <typename T>
 class NoBinario {
  protected:
-    T* dado;
-    NoBinario<T>* esquerda;
-    NoBinario<T>* direita;
-    std::vector<NoBinario<T>* > elementos;  //!< Elementos acessados durante
-                                            // o percurso realizado
+  T *dado;
+  NoBinario<T>* esquerda;
+  NoBinario<T>* direita;
+  std::vector<NoBinario<T>* > elementos;
+  typename std::vector<NoBinario<T>*>::iterator i = elementos.begin();
 
  public:
-    NoBinario<T>(const T& dado) {
-      this->dado = dado;
-      this->esquerda = NULL;
-      this->direita = NULL;
-    }
+  NoBinario<T>(const T& data) {
+    dado = new T(data);
+    esquerda = 0;
+    direita = 0;
+  }
 
-    virtual ~NoBinario();
+  virtual ~NoBinario() {
+    delete this->dado;
+  }
 
-    T* getDado() {
-      return this->dado;
-    }
+  T* getDado() {
+    return this->dado;
+  }
 
-    std::vector< NoBinario<T>* > getElementos() {
-      return this->elementos;
-    }
+  void setDado(T data) {
+    this->dado = *data;
+  }
 
-    NoBinario<T>* getEsquerda() {
-      return this->esquerda;
-    }
-
-    NoBinario<T>* getDireita() {
-      return this->direita;
-    }
-
-    T* busca(const T& dado, NoBinario<T>* arv) {
-      while (arv != NULL && arv->getDado() != dado) {
-        if (arv->getDado() < dado) {
-          arv = arv->getDireita();
-        } else {
-          arv = arv->getEsquerda();
-        }
-      }
-      return arv;
-    }
-
-    NoBinario<T>* inserir(const T& dado, NoBinario<T>* arv) {
-      NoBinario<T>* novo = new NoBinario<T>(dado); //NAO SEI INSTANCIAR SACARALHA
-      if (dado < arv->getDado()) {
-        if (arv->getEsquerda() == NULL) {
-          novo = arv;
-          // novo->dado = dado; //JÁ COLOQUEI O DADO PELO CONSTRUTOR NA PRIMEIRA LINHA
-          novo->esquerda = NULL;
-          novo->direita = NULL;
-          arv->esquerda = novo;
-        } else {
-          arv = inserir(arv->getEsquerda(), dado);
-        }
+  NoBinario<T>* inserir(const T& dado, NoBinario<T>* arv) {
+    NoBinario<T>* novoNoh = new NoBinario<T>(dado);
+    if (dado > *arv->getDado()) {
+      if (arv->getDireita() == 0) {
+        arv->direita = novoNoh;
       } else {
-        if (arv->getDireita() == NULL) {
-          novo = arv;
-          // novo->dado = dado; //JÁ COLOQUEI O DADO PELO CONSTRUTOR NA PRIMEIRA LINHA
-          novo->esquerda = NULL;
-          novo->direita = NULL;
-          arv->direita = novo;
+        arv->getDireita()->inserir(dado, arv->getDireita());
+      }
+    } else {
+      if (dado < *arv->getDado()) {
+        if (arv->getEsquerda() == 0) {
+          arv->esquerda = novoNoh;
         } else {
-          arv = inserir(arv->getDireita(), dado);
+          arv->getEsquerda()->inserir(dado, arv->getEsquerda());
         }
       }
     }
+    return novoNoh;
+  }
 
-    NoBinario<T>* remover(NoBinario<T>* arv, const T& dado) {
-      NoBinario<T>* temp = new NoBinario<T>(NULL);
-      NoBinario<T>* filho = new NoBinario<T>(NULL;)
-      if (arv == NULL) {
+  NoBinario<T>* getEsquerda() {
+    return this->esquerda;
+  }
+
+  NoBinario<T>* getDireita() {
+    return this->direita;
+  }
+
+  T* busca(const T& dado, NoBinario<T>* arv) {
+    if (arv != NULL) {
+      if (dado == *arv->getDado()) {
+        return arv->getDado();
+      } else {
+        if (dado > *arv->getDado()) {
+          arv->busca(dado, arv->getDireita());
+        } else {
+          arv->busca(dado, arv->getEsquerda());
+        }
+      }
+    } else {
+      throw("erro");
+    }
+  }
+
+  NoBinario<T>* minimo(NoBinario<T>* nodo) {
+    if (nodo->getEsquerda() == NULL) {
+      return nodo;
+    } else {
+      nodo->minimo(nodo->getEsquerda());
+    }
+  }
+
+  std::vector< NoBinario<T>* > getElementos() {
+    return this->elementos;
+  }
+
+  NoBinario<T>* remover(NoBinario<T>* arv, const T& dado) {
+    NoBinario<T>* temp = new NoBinario<T>(0);
+    NoBinario<T>* filho = new NoBinario<T>(0);
+    if (arv == NULL) {
+      return arv;
+    } else {
+      if (dado < *arv->getDado()) {
+        arv->esquerda = remover(arv->getEsquerda(), dado);
         return arv;
       } else {
-        if (dado < arv->getDado()) {
-          arv->getEsquerda() = remover(dado, arv->getEsquerda());
+        if (dado > *arv->getDado()) {
+          arv->direita = remover(arv->getDireita(), dado);
           return arv;
         } else {
-          if (dado > arv->getDado()) {
-            arv->getDireita() = remover(dado, arv->getDireita());
+          if (arv->getDireita() != NULL && arv->getEsquerda() != NULL) {
+            temp = minimo(arv->getDireita());
+            arv->dado = temp->getDado();
+            arv->direita = remover(arv->getDireita(), *arv->getDado());
             return arv;
           } else {
-            if (arv->getDireita() != NULL && arv->getEsquerda() != NULL) {
-              temp = minimo(arv->getDireita());
-              arv->dado = temp->getDado();
-              arv->direita = remover(arv->getDado(), arv->getDireita());
-              return arv;
+            temp = arv;
+            if (arv->getDireita() != NULL) {
+              filho = arv->getDireita();
+              return filho;
             } else {
-              temp = arv;
-              if (arv->getDireita() != NULL) {
-                filho = arv->getDireita();
+              if (arv->getEsquerda() != NULL) {
+                filho = arv->getEsquerda();
                 return filho;
               } else {
-                if (arv->getEsquerda() != NULL) {
-                  filho = arv->getEsquerda();
-                  return filho;
-                } else {
-                  // LIBERE ARV???
-                  return NULL;
-                }
+                arv->~NoBinario();
+                return NULL;
               }
             }
           }
         }
       }
     }
+  }
 
-    NoBinario<T>* minimo(NoBinario<T>* nodo) {
-      if (nodo->getEsquerda() == NULL) {
-        return nodo;
-      } else {
-        nodo->minimo(nodo->getEsquerda());
-      }
-    }
+  void addElemento(NoBinario<T>* nodo) {
+    elementos += nodo;
+  }
 
-    void preOrdem(NoBinario<T>* nodo) {
-      if (novo != NULL) {
-        printf(noto->getDado());
+  void preOrdem(NoBinario<T>* nodo) {
+    if (nodo != NULL) {
+      elementos.push_back(nodo);
+      if (nodo->getEsquerda() != NULL) {
         preOrdem(nodo->getEsquerda());
+      }
+      if (nodo->getDireita() != NULL) {
         preOrdem(nodo->getDireita());
       }
     }
+    elementos.end();
+  }
 
-    void emOrdem(NoBinario<T>* nodo) {
-      if (nodo != NULL) {
-        emOrdem(nodo->getEsquerda());
-        printf(nodp>getDado());
-        emOrdem(nodo->getDireita());
-      }
+  void emOrdem(NoBinario<T>* nodo) {
+    if (nodo->getEsquerda() != NULL) {
+      emOrdem(nodo->getEsquerda());
     }
+    elementos.push_back(nodo);
+    if (nodo->getDireita() != NULL) {
+      emOrdem(nodo->getDireita());
+    }
+    elementos.end();
+  }
 
-    void posOrdem(NoBinario<T>* nodo) {
-      if (nodo != NULL) {
+  void posOrdem(NoBinario<T>* nodo) {
+    if (nodo != NULL) {
+      if (nodo->getEsquerda() != NULL) {
         posOrdem(nodo->getEsquerda());
-        posOrdem(nodo->getDireita());
-        printf(nodo->getDado());
-
       }
+      if (nodo->getDireita() != NULL) {
+        posOrdem(nodo->getDireita());
+      }
+      elementos.push_back(nodo);
     }
+    elementos.end();
+  }
 };
 
 #endif
